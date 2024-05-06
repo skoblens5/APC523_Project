2163213ea04c775c3ef33a28349d146e452ee47a
clmartinblanc <cm6797@princeton.edu>
2024-05-06 13:18:23 -0400

    Structure python files

--- src/integrators.py
+++ src/integrators.py
@@ -1,5 +1,3 @@
-## Started overall formatting here. We may want to separate functions into different files so it is easier to work on
-
 ### Imports
 import numpy as np
 import matplotlib.pyplot as plt
@@ -32,21 +30,6 @@ def trapezoidal_integral(r, u):
         integral += area * average_height
     return integral
 
-def stability_study():
-    plt.figure(figsize=(12, 8))
-    dts = [0.00001, 0.00005, 0.0001]
-    colors = ['blue', 'green', 'red']
-    for dt, color in zip(dts, colors):
-        xi, times, bs, volumes, deltas = main_simulation(dt)
-        plt.plot(times, bs, label=f'DT={dt}', color=color, linewidth=2)
-
-    plt.xlabel('Time (s)', fontsize=14)
-    plt.ylabel('Scaling Factor b', fontsize=14)
-    plt.title('Evolution of Scaling Factor b Over Time', fontsize=16)
-    plt.legend(fontsize=12)
-    plt.grid(True, which='both', linestyle='--', linewidth=0.5)
-    plt.show()
-
 def precompute_gauss_legendre(n):
     """Precomputes Gauss-Legendre nodes and weights for given n."""
     nodes, weights = leggauss(n)
@@ -77,30 +60,3 @@ def simpsons_rule_radial(xi, delta):
     
     return integral
     
-def plot_similarity_solution(xi, times, bs, deltas, V0):
-    plt.figure(figsize=(10, 6))
-    cmap = cm.viridis  # Choose a colormap
-    norm = Normalize(vmin=min(times), vmax=max(times))  # Normalize times for color mapping
-
-    # Create a ScalarMappable for the colorbar
-    sm = cm.ScalarMappable(cmap=cmap, norm=norm)
-    sm.set_array([])
-
-    # Calculate how many points to skip to reduce the plot to about 100 points
-    step = max(1, len(times) // 100)
-
-    # Plot each profile with color mapped to time
-    for i in range(0, len(times), step):
-        time = times[i]
-        if time >= 0:  # Avoid division by zero and initial time
-            nu = xi * bs[i] * (3 * np.pi**3 * C**3 / (V0**3 * G * time))**(1/8)
-            f = deltas[i] / (3 * V0 * (1 / (16 * np.pi * C * G * time)))**(1/4)
-            plt.plot(nu, f, color=cmap(norm(time)))
-
-    plt.colorbar(sm, label='Time (s)')
-    plt.xlabel(r'$\nu$')
-    plt.ylabel(r'$f(\nu)$')
-    plt.title('Self-similar Solution Over Time')
-    plt.xlim([0,4])
-    plt.grid(True)
-    plt.show()
